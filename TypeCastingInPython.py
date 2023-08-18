a5df878cf9e97c1a984140aea1a0c6b3d007b81b
Kunal Bandale <bandalekunal@gmail.com>
2023-08-18 19:11:43 +0530

    Add files via upload

---
+++ TypeCastingInPython.py
@@ -0,0 +1,7 @@
+# type casting in python
+
+f = '5'
+print(int(f) + 1) # typecasted f string into integer
+g = 5
+print(g)
+
---
+++ operatorsInPython.py
@@ -0,0 +1,38 @@
+number_1 = 10
+number_2 = 2
+
+# Arithmetic Operators
+print("\n# Arithmetic Operators\n")
+print("Num1 + Num2 ", number_1 + number_2)
+print("Num1 - Num2 ", number_1 - number_2)
+print("Num1 * Num2 ", number_1 * number_2)
+print("Num1 / Num2 ", number_1 / number_2)
+print("Num1 // Num2 ", number_1 // number_2)
+print("Num1 ** Num2 ", number_1 ** number_2)
+print("Num1 % Num2 ", number_1 % number_2)
+
+# Assignment Operators
+print("\n#Assignment Operators\n")
+a = 4
+a += 2 # shorthand  a = a + 2
+print(a)
+
+# Comparison Operators
+print("\n# Comparison Operators\n")
+
+x = 8
+y = 3
+z = 8
+
+print(x > y)
+print(x < y)
+print(x != y)
+print(z == x)
+
+# Logical Operators
+print("\n# Logical Operators\n")
+print(x == z and x < y)
+print(x == z or x < y)
+print (not(False))
+print(not(True))
+
