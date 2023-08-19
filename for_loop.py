2fdd42ba2650eb9c1642b349b2ec5c4ff27f791c
Kunal Bandale <bandalekunal@gmail.com>
2023-08-19 17:44:03 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,7 @@
+for i in range(11):
+    print(i)
+# for loop with list
+print("list printing......")
+list1 = [1,2,3,4,3]
+for a in list1:
+    print(a)
---
+++ function_python.py
@@ -0,0 +1,16 @@
+def greetHello(name):
+    print("Hello " + name)
+
+greetHello("python")
+greetHello("py")
+
+def letterGenerator(name,date):
+    st = f"Hi , This is {name} i will not come to school at {date}"
+    print(st)
+letterGenerator("k","4th october")
+
+
+def avg(a,b):
+    return  (a+b/2)
+
+print(avg(3,4))
